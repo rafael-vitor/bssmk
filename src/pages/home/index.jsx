5b51a7466bb8f2b2d8b4c7345e023a5f1a42341b
ravi <rafael.vitor@jusbrasil.com.br>
2017-12-28 15:45:38 -0300

    Add basic home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react'
+
+const FixedMenuLayout = () => (
+  <div>
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item as='a' header>
+          Blood Sugar Sex Magik
+        </Menu.Item>
+        <Menu.Item as='a'>Home</Menu.Item>
+        <Menu.Item as='a'>Sobre</Menu.Item>
+      </Container>
+    </Menu>
+
+    <Container text style={{ marginTop: '7em' }}>
+      <Header as='h1'>Semantic UI React Fixed Template</Header>
+      <p>This is a basic fixed menu template using fixed size containers.</p>
+      <p>A text container is used for the main container, which is useful for single column layouts.</p>
+
+    </Container>
+
+  </div>
+)
+
+export default FixedMenuLayout
